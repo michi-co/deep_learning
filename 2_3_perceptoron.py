d5906c55dc1881893b280bda6cd1fa72daffe9f4
michiko <michitonton@yahoo.co.jp>
2025-02-26 22:46:45 +0900

    Update

--- 2_3_perceptoron.py
+++ 2_3_perceptoron.py
@@ -11,3 +11,16 @@ AND (0,0)
 AND (0,1)
 AND (1,0)
 AND (1,1)
+
+def NAND (x1, x2):
+    w1, w2, theta = -0.5, -0.5, -0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        print("0") 
+    elif tmp > theta:
+        print("1") 
+
+NAND (0,0)
+NAND (0,1)
+NAND (1,0)
+NAND (1,1)
